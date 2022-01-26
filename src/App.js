b1f3c04d4b72d1e1d0d33dd63abca78679e6efe6
Peter <warrenp11@gmail.com>
2022-01-26 09:42:32 -0500

    adding the logo to the navigation and the About and Contact sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
