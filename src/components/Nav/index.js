1c24d48706e754847bce111ad3575ef0f111115b
Peter <warrenp11@gmail.com>
2022-01-26 09:48:02 -0500

    map over the categories array to display on page, initialize onClick event

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 
 function Nav() {
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+  
+  function categorySelected() {
+    console.log("hello")
+  }
+  
   return (
     <header>
       <h2>
@@ -20,6 +38,11 @@ function Nav() {
           <li>
             <span>Contact</span>
           </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={categorySelected} >{category.name}</span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
