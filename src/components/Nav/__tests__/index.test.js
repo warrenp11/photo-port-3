7199ea0bdf47147ad1af12da8d0bdc3bed0fb60a
Peter <warrenp11@gmail.com>
2022-01-26 20:02:28 -0500

    init structure of Nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe("Nav component", () => {
+  // baseline test
+  // snapshot test
+});
