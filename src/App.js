a24ee95e62bf79fddbaf58c885817f3f23f007ce
Peter <warrenp11@gmail.com>
2022-01-28 13:07:21 -0500

    add contact form to app.js to display on homepage for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import About from "./components/About";
 import Nav from "./components/Nav";
 import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
@@ -29,6 +30,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
