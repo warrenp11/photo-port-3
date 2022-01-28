9c091e2d50c89204c1bf883573a4abb9ba72ce0f
Peter <warrenp11@gmail.com>
2022-01-28 13:13:43 -0500

    Initialize the State  of the form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 function ContactForm() {
   // React Hook
-  const [formState, setFormState] = useState();
+  // Feature of this Hook is the ability to initialize the values of the state, we want to clear the input fields on the component loading
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
 
   // JSX
   return (
@@ -12,17 +14,17 @@ function ContactForm() {
         {/* // name input */}
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={formState.name} />
         </div>
         {/* // email input */}
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={formState.name} />
         </div>
         {/* // message text area */}
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={formState.name} />
         </div>
         <button type="submit">Submit</button>
       </form>
