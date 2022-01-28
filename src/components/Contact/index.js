25013f767fc8584aaf12fceaa6a31c9eca056fea
Peter <warrenp11@gmail.com>
2022-01-28 13:18:09 -0500

    destructure formState object and use those constraints to assign initial state to the defaultValue

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 function ContactForm() {
-  // React Hook
-  // Feature of this Hook is the ability to initialize the values of the state, we want to clear the input fields on the component loading
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
+    // React Hook
+    // Feature of this Hook is the ability to initialize the values of the state, we want to clear the input fields on the component loading
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    // destructure the formState object into its named properties, name, email, and message
+    const { name, email, message } = formState;
 
   // JSX
   return (
@@ -14,17 +15,17 @@ function ContactForm() {
         {/* // name input */}
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={formState.name} />
+          <input type="text" name="name" defaultValue={name} />
         </div>
         {/* // email input */}
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={formState.name} />
+          <input type="email" name="email" defaultValue={email} />
         </div>
         {/* // message text area */}
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={formState.name} />
+          <textarea name="message" rows="5" defaultValue={message} />
         </div>
         <button type="submit">Submit</button>
       </form>
